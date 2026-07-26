1d23f7b6271cd6a717a4978c0c97647a973caa0a
Pedro Ramos <pedroramos327698@gmail.com>
2026-07-26 02:15:16 -0300

    First commit. Pedidos microsservice start.

---
+++ pedido/src/test/java/p/ramos/ms/pedido/PedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package p.ramos.ms.pedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
