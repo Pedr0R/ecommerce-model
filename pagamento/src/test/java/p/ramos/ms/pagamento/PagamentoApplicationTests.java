a9a06fa35d273830e58079f694827816aff60c61
Pedro Ramos <pedroramos327698@gmail.com>
2026-07-26 02:30:39 -0300

    Inicializa todos os microsservicos e arquivos auxiliares

---
+++ pagamento/src/test/java/p/ramos/ms/pagamento/PagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package p.ramos.ms.pagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
